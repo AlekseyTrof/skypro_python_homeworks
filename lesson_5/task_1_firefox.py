6343961c3c6433b9ae1b40f5c01a0d2c33b8fea9
AlekseyTrof <alekcey.trofimov.99@gmail.com>
2024-04-18 18:36:08 +0300

    add 5 homework

---
+++ lesson_5/task_1_firefox.py
@@ -0,0 +1,18 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+
+driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
+
+click = driver.find_element(By.CSS_SELECTOR, '[onclick="addElement()"]')
+for x in range (5):
+    sleep(1)
+    click.click()
+
+find_add = driver.find_elements(By.CSS_SELECTOR, 'button.added-manually')
+print("Найдено", len(find_add), "кнопок Delete")
+
+sleep(2)  # успеть посмотреть что произошло в браузере
+driver.quit()
---
+++ lesson_5/task_6_firefox.py
@@ -0,0 +1,23 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+
+driver.get("http://the-internet.herokuapp.com/login")
+sleep(1)
+
+username = driver.find_element(By.CSS_SELECTOR, '#username')
+username.send_keys("tomsmith")
+sleep(1)
+
+password = driver.find_element(By.CSS_SELECTOR, '#password')
+password.send_keys("SuperSecretPassword!")
+sleep(1)
+
+login = driver.find_element(By.CSS_SELECTOR, '.radius')
+login.click()
+
+sleep(1)
+driver.quit()
